8b2e71cf3321758aba15d973b4b39a8c1e74266b
andrewogren42 <andrewogren42@gmail.com>
2026-03-10 13:07:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
+#triggering API rebuild
 # ─────────────────────────────────────────────
 #  GAME + MODEL (copied from training code)
 # ─────────────────────────────────────────────
